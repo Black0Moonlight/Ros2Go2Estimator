5be17bee250c37b88038adaa846d175267f42b67
Blamlight <2417873446@qq.com>
2025-05-06 23:26:43 +0900

    添加连接实体机器人的代码

--- src/fusion_estimator/src/fusion_estimator_node.cpp
+++ src/fusion_estimator/src/fusion_estimator_node.cpp
@@ -8,6 +8,7 @@
 #include <urdf/model.h>
 #include <unitree_legged_msgs/LowState.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Float32MultiArray.h>
 
 #include <ros/ros.h>
 
@@ -27,8 +28,11 @@ public:
         ros::NodeHandle nh;
         
         // Initialize publishers and subscribers
-        lowstate_subscriber = nh.subscribe("/go1/lowstate", 10, 
-                                           &FusionEstimatorNode::LowStateCallback, this);
+        // lowstate_subscriber = nh.subscribe("/go1/lowstate", 10, 
+        //                                    &FusionEstimatorNode::LowStateCallback, this);
+
+        udpstate_subscriber = nh.subscribe("/smxfe/low_state_array", 10, 
+                                            &FusionEstimatorNode::UdpStateCallback, this);        
         // Reset subscriber
         reset_subscriber = nh.subscribe<std_msgs::Bool>("/smxfe/reset_pose", 1, &FusionEstimatorNode::ResetCallback, this);
 
@@ -76,6 +80,7 @@ private:
     ros::Publisher smxfe_publisher;
     ros::Publisher smx_imu_publisher;
     ros::Subscriber lowstate_subscriber;
+    ros::Subscriber udpstate_subscriber;
     ros::Subscriber reset_subscriber;
 
     std::shared_ptr<DataFusion::SensorIMUAcc> Sensor_IMUAcc; 
@@ -292,25 +297,169 @@ private:
         smx_imu_publisher.publish(imu_msg);
     }
 
+    void UdpStateCallback(const std_msgs::Float32MultiArray::ConstPtr& msg)
+    {
+        if (msg->data.size() != 37)
+        {
+            ROS_WARN("Received Float32MultiArray size mismatch. Expected 37, got %zu", msg->data.size());
+            return;
+        }
+
+        const auto& data = msg->data;
+        fusion_estimator::FusionEstimatorTest fusion_msg;
+        fusion_msg.stamp = ros::Time::now();
+
+        double LatestMessage[3][100] = {0};
+        static double LastMessage[3][100] = {0};
+        double CurrentTimestamp = fusion_msg.stamp.toSec();
+
+        // -------------------------- 赋值 imu --------------------------
+        for(int i = 0; i < 3; i++){
+            fusion_msg.data_check_a[0+i] = data[i];         // accelerometer
+            fusion_msg.data_check_a[3+i] = data[3+i];       // rpy
+            fusion_msg.data_check_a[6+i] = data[6+i];       // gyro
+
+            LatestMessage[0][3*i+2] = data[i];              // accelerometer for IMUAcc
+            LatestMessage[1][3*i]   = data[3+i];            // rpy for IMUMagGyro
+            LatestMessage[1][3*i+1] = data[6+i];            // gyro for IMUMagGyro
+        }
+
+        // Sensor Data Handle
+        for(int i = 0; i < 9; i++)
+        {
+            if(LastMessage[0][i] != LatestMessage[0][i])
+            {
+                Sensor_IMUAcc->SensorDataHandle(LatestMessage[0], CurrentTimestamp);
+                for(int j = 0; j < 9; j++) LastMessage[0][j] = LatestMessage[0][j];
+                break;
+            }
+        }
+
+        for(int i = 0; i < 9; i++)
+        {
+            if(LastMessage[1][i] != LatestMessage[1][i])
+            {
+                Sensor_IMUMagGyro->SensorDataHandle(LatestMessage[1], CurrentTimestamp);
+                for(int j = 0; j < 9; j++) LastMessage[1][j] = LatestMessage[1][j];
+                break;
+            }
+        }
+
+        for(int i=0; i<9; i++){
+            fusion_msg.estimated_xyz[i] = StateSpaceModel1_Sensors[0]->EstimatedState[i];
+            fusion_msg.estimated_rpy[i] = StateSpaceModel1_Sensors[1]->EstimatedState[i];
+        }
+
+        // -------------------------- 赋值腿部 --------------------------
+        for(int LegNumber = 0; LegNumber < 4; LegNumber++)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                fusion_msg.data_check_b[LegNumber*3+i] = data[9 + LegNumber*3 + i];   // q
+                fusion_msg.data_check_c[LegNumber*3+i] = data[21 + LegNumber*3 + i];  // dq
+
+                LatestMessage[2][LegNumber*3+i]        = data[9 + LegNumber*3 + i];
+                LatestMessage[2][12 + LegNumber*3+i]   = data[21 + LegNumber*3 + i];
+            }
+            LatestMessage[2][24 + LegNumber] = data[33 + LegNumber];
+            fusion_msg.others[LegNumber]     = data[33 + LegNumber];
+        }
+
+        for(int i = 0; i < 28; i++)
+        {
+            if(LastMessage[2][i] != LatestMessage[2][i])
+            {
+                Sensor_Legs->SensorDataHandle(LatestMessage[2], CurrentTimestamp);
+                for(int j = 0; j < 28; j++) LastMessage[2][j] = LatestMessage[2][j];
+                break;
+            }
+        }
+
+        for(int i=0; i<4; i++){
+            for(int j=0; j<3; j++){
+                fusion_msg.data_check_d[3 * i + j]       = StateSpaceModel1_Sensors[0]->Double_Par[6 * i + j];
+                fusion_msg.data_check_e[3 * i + j]       = StateSpaceModel1_Sensors[0]->Double_Par[24 + 6 * i + j];
+                fusion_msg.feet_based_position[3*i + j]  = StateSpaceModel1_Sensors[0]->Double_Par[48 + 6 * i + j];
+                fusion_msg.feet_based_velocity[3*i + j]  = StateSpaceModel1_Sensors[0]->Double_Par[48 + 6 * i + j + 3];
+            }
+        }
+
+        // -------------------------- 发布消息 --------------------------
+        fe_test_publisher.publish(fusion_msg);
+
+        nav_msgs::Odometry SMXFE_odom;
+        SMXFE_odom.header.stamp = fusion_msg.stamp;
+        SMXFE_odom.header.frame_id = "base";
+        SMXFE_odom.child_frame_id = "trunk";
+
+        SMXFE_odom.pose.pose.position.x = fusion_msg.estimated_xyz[0];
+        SMXFE_odom.pose.pose.position.y = fusion_msg.estimated_xyz[3];
+        SMXFE_odom.pose.pose.position.z = fusion_msg.estimated_xyz[6];
+
+        tf::Quaternion q;
+        q.setRPY(fusion_msg.estimated_rpy[0], fusion_msg.estimated_rpy[3], fusion_msg.estimated_rpy[6]);
+
+        SMXFE_odom.pose.pose.orientation.x = q.x();
+        SMXFE_odom.pose.pose.orientation.y = q.y();
+        SMXFE_odom.pose.pose.orientation.z = q.z();
+        SMXFE_odom.pose.pose.orientation.w = q.w();
+        smxfe_publisher.publish(SMXFE_odom);
+
+        tf::Transform transform;
+        transform.setOrigin(tf::Vector3(
+            fusion_msg.estimated_xyz[0],
+            fusion_msg.estimated_xyz[3],
+            fusion_msg.estimated_xyz[6]
+        ));
+        transform.setRotation(q);
+        tf_broadcaster->sendTransform(tf::StampedTransform(
+            transform,
+            fusion_msg.stamp,
+            "odom",
+            "base"
+        ));
+
+        sensor_msgs::Imu imu_msg;
+        imu_msg.header.stamp = fusion_msg.stamp;
+        imu_msg.header.frame_id = "trunk";
+        imu_msg.linear_acceleration.x = data[0];
+        imu_msg.linear_acceleration.y = data[1];
+        imu_msg.linear_acceleration.z = data[2];
+        imu_msg.angular_velocity.x = data[6];
+        imu_msg.angular_velocity.y = data[7];
+        imu_msg.angular_velocity.z = data[8];
+        tf::Quaternion imu_q;
+        imu_q.setRPY(data[3], data[4], data[5]);
+        imu_msg.orientation.x = imu_q.x();
+        imu_msg.orientation.y = imu_q.y();
+        imu_msg.orientation.z = imu_q.z();
+        imu_msg.orientation.w = imu_q.w();
+        imu_msg.orientation_covariance[0] = -1;
+        imu_msg.angular_velocity_covariance[0] = -1;
+        imu_msg.linear_acceleration_covariance[0] = -1;
+        smx_imu_publisher.publish(imu_msg);
+    }
+
+    
     void ResetCallback(const std_msgs::Bool::ConstPtr& msg)
     {
         if (msg->data)
         {
             ROS_WARN("Received reset request. Resetting estimator states...");
 
-            for (auto* model : StateSpaceModel1_Sensors)
-            {
-                for (int i = 0; i < 9; ++i)
-                    model->EstimatedState[i] = 0;
+            // for (auto* model : StateSpaceModel1_Sensors)
+            // {
+            //     for (int i = 0; i < 9; ++i)
+            //         model->EstimatedState[i] = 0;
 
-                for (int i = 0; i < 72; ++i)
-                    model->Double_Par[i] = 0;
+            //     for (int i = 0; i < 72; ++i)
+            //         model->Double_Par[i] = 0;
 
-            }
-            for(int i=0; i<4; i++)
-            {
-                Sensor_Legs->FootfallPositionRecordIsInitiated[i] = 0;
-            }
+            // }
+            // for(int i=0; i<4; i++)
+            // {
+            //     Sensor_Legs->FootfallPositionRecordIsInitiated[i] = 0;
+            // }
             StateSpaceModel1_Sensors[0]->EstimatedState[0] = 0;
             StateSpaceModel1_Sensors[0]->EstimatedState[3] = 0;
             StateSpaceModel1_Sensors[0]->EstimatedState[6] = 0;
